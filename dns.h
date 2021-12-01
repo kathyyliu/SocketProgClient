aaa3d0dc8110e259a2c68b415420a64c6e3edba0
jonah-winchell <93216462+jonah-winchell@users.noreply.github.com>
2021-11-30 19:07:54 -0800

    Add files via upload

---
+++ dns.h
@@ -0,0 +1,13 @@
+//
+// Created by Jonah Winchell on 11/30/21.
+//
+
+#ifndef DNS_H
+#define DNS_H
+
+char* ngethostbyname (unsigned char*, int);
+void ChangetoDnsNameFormat (unsigned char*, unsigned char*);
+unsigned char* ReadName (unsigned char*, unsigned char*, int*);
+void get_dns_servers();
+
+#endif
